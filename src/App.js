1d9e7262183dc890fe553282aad7df2ce7068d54
BanurekhaMohan <bmohan@deloitte.co.uk>
2023-05-12 18:12:23 +0100

    Update react to functional components

--- src/App.js
+++ src/App.js
@@ -1,66 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import Accordion from "react-bootstrap/Accordion";
 import Button from "react-bootstrap/Button";
 import { getData } from "./actions/getData";
-import { connect } from "react-redux";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      content: [{ Positive: [] }, { Negative: [] }, { Neutral: [] }],
-      PoisitivePage: 1,
-      NegativePage: 1,
-      NeutralPage: 1
-    };
-    this.renderCardtext = this.renderCardtext.bind(this);
-    this.renderPagenumbers = this.renderPagenumbers.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
+function App() {
+  const [contentVal, setContent] = useState([
+    { Positive: [] },
+    { Negative: [] },
+    { Neutral: [] },
+  ]);
+  const [PoisitivePage, setPoisitivePage] = useState(1);
+  const [NegativePage, setNegativePage] = useState(1);
+  const [NeutralPage, setNeutralPage] = useState(1);
+  const apiData = useSelector((state) => state.getDataReducer.apiData);
+  const dispatch = useDispatch();
+  console.log("apiData111", apiData);
+  useEffect(() => {
 
-  componentDidMount() {
-    let Sentiment = require("sentiment");
-    let sentiment = new Sentiment();
-    this.props.dispatch(getData()).then(() => {
-      this.props.apiData.forEach((content, i) => {
-        if (content.review_body + content.review_headline) {
-          let res = sentiment.analyze(
-            content.review_body + content.review_headline
-          );
-          let temp, category, stateCopy;
-          stateCopy = Object.assign({}, this.state);
-          switch (true) {
-            case res.score < 0 ||
-              content.star_rating === "1" ||
-              content.star_rating === "2":
-              category = 1;
-              temp = "Negative";
-              break;
-            case res.score > 0 || content.star_rating === "5":
-              category = 0;
-              temp = "Positive";
-              break;
-            case res.score === 0:
-              category = 2;
-              temp = "Neutral";
-              break;
-            default:
-              break;
-          }
-          stateCopy.content[category][temp].push({
-            Title: content.product_title,
-            review_body: content.review_body,
-            review_headline: content.review_headline,
-            Date: content.review_date,
-            Sentiment_Score: res.score
-          });
-          this.setState(stateCopy);
-        }
-      });
-    });
+    dispatch(getData());
 
     /* Data format
       [
@@ -69,32 +30,76 @@ class App extends React.Component {
         {"Neutral":[{product_title:"",review_body:"",score:null,category:""},{product_title:"",review_body:"",score:null,category:""}]}
       ]
       */
-  }
+  }, [dispatch]);
 
-  handleClick(event) {
+  useEffect(() => {
+    let Sentiment = require("sentiment");
+    let sentiment = new Sentiment();
+    console.log("apiData", apiData);
+    apiData.forEach((content, i) => {
+      if (content.review_body + content.review_headline) {
+        let res = sentiment.analyze(
+          content.review_body + content.review_headline
+        );
+        let temp, category, stateCopy;
+        stateCopy = Object.assign({}, contentVal);
+        switch (true) {
+          case res.score < 0 ||
+            content.star_rating === "1" ||
+            content.star_rating === "2":
+            category = 1;
+            temp = "Negative";
+            break;
+          case res.score > 0 || content.star_rating === "5":
+            category = 0;
+            temp = "Positive";
+            break;
+          case res.score === 0:
+            category = 2;
+            temp = "Neutral";
+            break;
+          default:
+            break;
+        }
+        console.log("stateCopy", stateCopy, category, temp);
+        stateCopy[category][temp].push({
+          Title: content.product_title,
+          review_body: content.review_body,
+          review_headline: content.review_headline,
+          Date: content.review_date,
+          Sentiment_Score: res.score,
+        });
+        setContent((prev) => [...prev, stateCopy]);
+      }
+    });
+  }, [apiData]);
+
+  function handleClick(event) {
     let category = event.target.getAttribute("category");
-    if (category == "Positive") {
-      this.setState({ PoisitivePage: Number(event.target.id) });
-    } else if (category == "Negative") {
-      this.setState({ NegativePage: Number(event.target.id) });
-    } else if (category == "Neutral") {
-      this.setState({ NeutralPage: Number(event.target.id) });
+    if (category === "Positive") {
+      setPoisitivePage(Number(event.target.id));
+    } else if (category === "Negative") {
+      setNegativePage(Number(event.target.id));
+    } else if (category === "Neutral") {
+      setNeutralPage(Number(event.target.id));
     }
   }
 
-  renderCardtext(contentVal, category) {
+  function renderCardtext(contentVal, category) {
     // Logic for displaying current Items
     let indexOfLastItem;
-    if (category == "Positive") {
-      indexOfLastItem = this.state.PoisitivePage * 3;
-    } else if (category == "Negative") {
-      indexOfLastItem = this.state.NegativePage * 3;
-    } else if (category == "Neutral") {
-      indexOfLastItem = this.state.NeutralPage * 3;
+    if (category === "Positive") {
+      indexOfLastItem = PoisitivePage * 3;
+    } else if (category === "Negative") {
+      indexOfLastItem = NegativePage * 3;
+    } else if (category === "Neutral") {
+      indexOfLastItem = NeutralPage * 3;
     }
     const indexOfFirstItem = indexOfLastItem - 3;
-    const currentItems = contentVal.slice(indexOfFirstItem, indexOfLastItem);
-    return currentItems.map((content, i) => {
+    console.log('contentVal', contentVal);
+    const currentItems = Array.isArray(contentVal) ? contentVal.slice(indexOfFirstItem, indexOfLastItem) : contentVal;
+    console.log("currentItems", currentItems);
+    return Array.isArray(currentItems) && currentItems.map((content, i) => {
       return (
         <Accordion defaultActiveKey="0" key={i}>
           <Card>
@@ -118,95 +123,83 @@ class App extends React.Component {
     });
   }
 
-  renderPagenumbers(contentVal, category) {
+  function renderPagenumbers(contentVal, category) {
     // Logic for displaying page numbers
     const pageNumbers = [];
     for (let i = 1; i <= Math.ceil(contentVal.length / 3); i++) {
       pageNumbers.push(i);
     }
 
-    return pageNumbers.map(number => {
+    return pageNumbers.map((number) => {
       return (
-        <li
-          key={number}
-          id={number}
-          category={category}
-          onClick={this.handleClick}
-        >
+        <li key={number} id={number} category={category} onClick={handleClick}>
           {number}
         </li>
       );
     });
   }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">Sentiment Analysis</header>
-        <section className="jumbotron text-center">
-          <div className="container">
-            <h1 className="jumbotron-heading">
-              Sentiment Analysis for Amazon Customer reviews
-            </h1>
-            <p className="lead text-muted">
-              A CI/CD workflow enabled React-bootstrap app which analyses the
-              sentiment of amazon customer reviews is deployed in AWS Amplify
-              development platform. Customer reviews dataset found
-              <a
-                href="https://s3.amazonaws.com/amazon-reviews-pds/tsv/sample_us.tsv"
-                target="_blank"
-              >
-                {" "}
-                here.{" "}
-              </a>
-              Github Link
-              <a
-                href="https://github.com/banurekhaMohan279/AmazonReviews-Analyser"
-                target="_blank"
-              >
-                {" "}
-                here.
-              </a>
-            </p>
-          </div>
-        </section>
-        <div className="album py-5 bg-light">
-          <div className="container">
-            <CardDeck>
-              {this.state.content.map((content, i) => {
-                let category = Object.keys(content)[0];
-                let contentVal = content[category];
-                let imgname = Object.keys(content)[0].toLowerCase();
-                return (
-                  <Card key={i} style={{ minWidth: "18rem" }}>
-                    <Card.Img
-                      variant="top"
-                      src={"/AmazonReviews-Analyser/img/" + imgname + ".png"}
-                    />
-                    <Card.Body>
-                      <Card.Title>{category + " Comments"}</Card.Title>
-                      {this.renderCardtext(contentVal, category)}
-                    </Card.Body>
-                    <ul id="page-numbers">
-                      {this.renderPagenumbers(contentVal, category)}
-                    </ul>
-                  </Card>
-                );
-              })}
-            </CardDeck>
-          </div>
+  return (
+    <div className="App">
+      <header className="App-header">Sentiment Analysis</header>
+      <section className="jumbotron text-center">
+        <div className="container">
+          <h1 className="jumbotron-heading">
+            Sentiment Analysis for Amazon Customer reviews
+          </h1>
+          <p className="lead text-muted">
+            A CI/CD workflow enabled React-bootstrap app which analyses the
+            sentiment of amazon customer reviews is deployed in AWS Amplify
+            development platform. Customer reviews dataset found
+            <a
+              href="https://s3.amazonaws.com/amazon-reviews-pds/tsv/sample_us.tsv"
+              target="_blank" without rel="noreferrer"
+            >
+              {" "}
+              here.{" "}
+            </a>
+            Github Link
+            <a
+              href="https://github.com/banurekhaMohan279/AmazonReviews-Analyser"
+              target="_blank" without rel="noreferrer"
+            >
+              {" "}
+              here.
+            </a>
+          </p>
         </div>
-        <footer className="App-footer">
-          @Copyrights Reserved 2020. Contact banurekha279@gmail.com for queries.
-        </footer>
+      </section>
+      <div className="album py-5 bg-light">
+        <div className="container">
+          <CardDeck>
+            {contentVal.map((content, i) => {
+              console.log("contentcontentcontentcontent", content);
+              let category = Object.keys(content)[0];
+              let contentInner = content[category];
+              let imgname = Object.keys(content)[0].toLowerCase();
+              return (
+                <Card key={i} style={{ minWidth: "18rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src={"/AmazonReviews-Analyser/img/" + imgname + ".png"}
+                  />
+                  <Card.Body>
+                    <Card.Title>{category + " Comments"}</Card.Title>
+                    {renderCardtext(contentInner, category)}
+                  </Card.Body>
+                  <ul id="page-numbers">
+                    {renderPagenumbers(contentInner, category)}
+                  </ul>
+                </Card>
+              );
+            })}
+          </CardDeck>
+        </div>
       </div>
-    );
-  }
+      <footer className="App-footer">
+        @Copyrights Reserved 2023. Contact banurekha279@gmail.com for queries.
+      </footer>
+    </div>
+  );
 }
 
-function mapStateToProps(state) {
-  return {
-    apiData: state.getDataReducer.apiData
-  };
-}
-export default connect(mapStateToProps)(App);
+export default App;
